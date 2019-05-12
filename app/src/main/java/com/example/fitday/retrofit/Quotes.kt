b1e8b1f402dab472a344fb97fcfabb2b8b788501
ketrab321 <37443550+ketrab321@users.noreply.github.com>
2019-05-12 20:10:07 +0200

    Implemented gallery

--- app/src/main/java/com/example/fitday/retrofit/Quotes.kt
+++ app/src/main/java/com/example/fitday/retrofit/Quotes.kt
@@ -2,10 +2,11 @@ package com.example.fitday.retrofit
 
 data class Quotes (
     var quote : String,
-    var author : String,
     var length : String,
+    var author : String,
     var tags : List<Any>,
     var category : String,
-    var title : String,
     var date : String,
-    var id : Int){}
+
+    var title : String,
+    var id : String){}

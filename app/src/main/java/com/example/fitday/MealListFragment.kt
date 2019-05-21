5a82a9f65e95a666d2838e7a8d05d14ba8a4facd
Piotr Andrzejewski <piotr.andrzejewski00@gmail.com>
2019-05-21 12:38:19 +0200

    add all files to complete last commit

--- app/src/main/java/com/example/fitday/MealListFragment.kt
+++ app/src/main/java/com/example/fitday/MealListFragment.kt
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.meal_list.*
 
 class MealListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -13,5 +14,11 @@ class MealListFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        addMealButton.setOnClickListener {
+            val newFragment = FireMissilesDialogFragment()
+            newFragment.show(fragmentManager, "missiles");
+        }
+
     }
+
 }

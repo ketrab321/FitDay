eb7122ad054073e40870f35b1493e4eb94a92fd8
Jacket44 <mateusz.trzeciak44@gmail.com>
2019-05-26 11:58:44 +0200

    Changed icon to Legacy and cleared useless menus.

--- app/src/main/java/com/example/fitday/MainActivity.kt
+++ app/src/main/java/com/example/fitday/MainActivity.kt
@@ -157,48 +157,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            R.id.action_change_body_parameters -> {
-                val myintent = Intent(this, BodyParamsForm::class.java )
-                startActivityForResult(myintent, CHANGE_BODY_PARAMETERS_REQUEST_CODE)
-                return true
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
             R.id.nav_gallery -> {
                 var intent = Intent(this,Gallery::class.java)
                 startActivity(intent)
             }
-            R.id.nav_slideshow -> {
-
-            }
             R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
+                var intent = Intent(this,BodyParamsForm::class.java)
+                startActivity(intent)
             }
         }
 
